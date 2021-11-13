8b8b571c55622334c2896b13509b7333d2bc78e2
Hong Duc Cai <duchongcai@gmail.com>
2021-11-13 16:25:11 +0100

    Initial Commit

---
+++ chat app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat app
+//
+//  Created by Duc hong cai on 13/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
