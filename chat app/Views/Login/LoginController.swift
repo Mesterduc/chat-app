c1c879fb83bc158c87f4a7de938ace18076e77ce
Hong Duc Cai <duchongcai@gmail.com>
2021-11-13 17:42:37 +0100

    update  pods

--- chat app/Views/Login/LoginController.swift
+++ chat app/Views/Login/LoginController.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class LoginController: UIViewController {
+    let db = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        
+        let docRef = db.collection("hej").document("hej2")
+
+        docRef.getDocument { (document, error) in
+//            print(document)
+            if let document = document, document.exists {
+                let dataDescription = document.data()
+                print("Document data: \(dataDescription!.values)")
+            } else {
+                print("Document does not exist")
+            }
+        }
     }
 
 
